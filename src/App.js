8c6314c1d4c2c7870b1bd68ea9d6ce641fae1c42
Zhichong Li <leezhc@gmail.com>
2017-08-18 23:56:28 -0700

    initial router  and lint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component }     from 'react';
-import {Route}                  from 'react-router-dom';
+import React, { Component }  from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Default                  from './pages/Default';
 import Category                 from './pages/Category';
 import PostDetail               from './pages/PostDetail';
@@ -8,10 +8,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path="/" component={Default}/>
-        <Route path="/category" render={({history}) => (
-          <Category />
-        )}/>
+        <Router>
+          <div>
+            <Route path="/category" render={({history}) => (
+              <Category/>
+            )}/>
+            <Route exact path="/" component={Default}/>
+          </div>
+        </Router>
       </div>
     );
   }
