d05b8c2a901012aad91b983a1a006c9a51331fd5
Zhichong Li <leezhc@gmail.com>
2017-08-19 00:58:05 -0700

    init redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component }  from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Default                  from './pages/Default';
-import Category                 from './pages/Category';
-import PostDetail               from './pages/PostDetail';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Default from './pages/Default';
+import Category from './pages/Category';
+import PostDetail from './pages/PostDetail';
+import Navbar from './components/Navbar';
+import { createStore } from 'redux';
+import reducer from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  reducer
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          <div>
-            <Route path="/category" render={({history}) => (
-              <Category/>
-            )}/>
-            <Route exact path="/" component={Default}/>
-          </div>
-        </Router>
+        <Provider store={store}>
+          <Router>
+            <div>
+              <Navbar/>
+              <Route exact path="/" component={Default}/>
+              <Route path="/category" render={({history}) => (
+                <Category/>
+              )}/>
+            </div>
+          </Router>
+        </Provider>
       </div>
     );
   }
