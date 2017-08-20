2a772f57cf64557bc64dd551bbd3dc17aece34aa
Zhichong Li <leezhc@gmail.com>
2017-08-20 00:52:47 -0700

    navbar active style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
 
 const list = [
   {
-    name: "Default",
-    url: "/default"
+    name: 'Default',
+    url: '/default'
   },
   {
-    name: "Category",
-    url: "/category"
+    name: 'Category',
+    url: '/category'
   }
 ];
 
-const Navbar = () => {
-  return (
-    <div>
-      <nav className='navbar'>
-        <span>Readable App</span>
-        {list.map(item => (
-          <span key={item.name} className='navitem'> | <Link className='navitem' to={item.url}>{item.name}</Link></span>
-        ))}
-      </nav>
-      
-    </div>
-  );
-};
 
-export default Navbar;
+class Navbar extends Component {
+  static propTypes = {
+    match: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired
+  }
+  render() {
+    return (
+      <div>
+        <nav className='navbar'>
+          <span>Readable App</span>
+          {list.map(item => {
+            const match = this.props.location.pathname === item.url;
+            console.log(match);
+            return (
+              <span key={item.name} className='navitem'> | <Link className={match?'activrbar':'navitem'} to={item.url}>{item.name}</Link></span>
+            )}
+          )}
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Navbar);
