0bef93682b5e680f876f686bbce954c4a1b3081a
Zhichong Li <leezhc@gmail.com>
2017-08-28 22:36:00 -0700

    cat bar interactive with url params

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
 
-const CategoryList = ({categories}) => {
-  return (
-    <div className='row cat-container'>
-      <div className='col-sm-3'>Categories:</div>
-      {
-        categories.map((cat, i) => (
-          <div key={i} className='col-sm-3'>
-            <Link to={`/category/${cat.path}`}><span>{cat.name}</span></Link>
-          </div>
-        ))
-      }
-    </div>
-  )
+class CategoryList extends Component {
+  render() {
+    const {categories, match} = this.props;
+    let routeCat = '';
+    if(match.params.cat) {
+      routeCat = match.params.cat
+    }
+    return (
+      <div className='row cat-container'>
+        <div className='col-sm-3'>Categories:</div>
+        {
+          categories.map((cat, i) => (
+            <div key={i} className='col-sm-3'>
+              {
+                cat.name == routeCat ?
+                  <span>{cat.name}</span> :
+                  <Link to={`/category/${cat.path}`}><span>{cat.name}</span></Link>
+              }
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
 }
 
-export default CategoryList;
+export default withRouter(connect()(CategoryList));
